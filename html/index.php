5ff3a7217f18aec51a0c07a4d477d42c77cfa0ee
Per Persson <per.persson@exertisztorm.com>
2015-10-15 14:12:13 +0200

    Test db connection

--- html/index.php
+++ html/index.php
@@ -5,4 +5,5 @@ echo '<pre>';
 print_r($_ENV);
 echo '</pre>';
 
-$pdo = new \PDO('');
+$pdo = new \PDO("mysql:host='{$_ENV['DB_PORT_3306_TCP_ADDR']}'", "root", $_ENV['DB_ENV_MYSQL_ROOT_PASSWORD']);
+var_dump($pdo);
